6a8a80871d876f8c1e5eeb143ca9c745b8aaffde
zhongxiaodong <zxdzyadmin@yonyou.com>
2018-09-29 15:45:52 +0800

    dd

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
 import 'babel-polyfill';
 import React, { Component } from 'react';
-import {Button} from 'antd';
-import logo from './logo.svg';
-import './App.css';
+import { Layout, Menu, Breadcrumb } from 'antd';
+
+const { Header, Content, Footer } = Layout;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button>ssss</Button>
+      <div>
+        <Layout>
+          <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+            <Menu
+              theme="linght"
+              mode="horizontal"
+              defaultSelectedKeys={['2']}
+              style={{ lineHeight: '64px' }}
+            >
+              <Menu.Item key="1">nav 1</Menu.Item>
+              <Menu.Item key="2">nav 2</Menu.Item>
+              <Menu.Item key="3">nav 3</Menu.Item>
+              <Menu.Item key="4"></Menu.Item>
+              <Menu.Item key="5"></Menu.Item>
+              <Menu.Item key="6"></Menu.Item>
+              <Menu.Item key="7"></Menu.Item>
+              <Menu.Item key="8"></Menu.Item>
+            </Menu>
+          </Header>
+          <Content style={{ padding: '0 30px', marginTop: 50 }}>
+            <Breadcrumb style={{ margin: '10px 0' }}>
+              <Breadcrumb.Item>Home</Breadcrumb.Item>
+              <Breadcrumb.Item>List</Breadcrumb.Item>
+              <Breadcrumb.Item>App</Breadcrumb.Item>
+            </Breadcrumb>
+            <div style={{ background: '#fff', padding: 24, minHeight: 380 }}>Content</div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>
+            Ant Design ©2018 Created by Ant UED
+          </Footer>
+        </Layout>,
       </div>
     );
   }
